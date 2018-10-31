ac2d0e6ee03853b5b841902223c32cb75384a247
marypiper <marypiper@users.noreply.github.com>
2018-10-31 15:50:26 -0400

    Update clustering_pre_regress.R

--- scRNAseq/scripts/clustering_pre_regress.R
+++ scRNAseq/scripts/clustering_pre_regress.R
@@ -1,8 +1,6 @@
-# Usage: this Rscript is using Seurat to subset data for a particular sample given as a command line argument, then will perform normalization, calculation of variable genes and significant PCs, finally it will score cells for cell cycle. The output is ready for the regress.R script. 
+# Usage: this Rscript is using Seurat to perform normalization, calculation of variable genes and significant PCs, finally it will score cells for cell cycle. The output is ready for the regress.R script. 
 
-# The script expects as input the name of the interestingGroup used to subset samples. If prefer to use a different column in metadata to subset, can change line 22.
-
-# To run:  Rscript name_of_script "interestingGroup_name"
+# To run:  Rscript name_of_script
 
 library(Seurat)
 library(tidyverse)
@@ -13,13 +11,6 @@ set.seed(1454944673L)
 # Load pre_regressed Seurat object                                                                                                
 pre_regressed_seurat <- readRDS(file.path(data_dir, "seurat_raw.rds"))
 
-# Use command line argument to specify sample extracted
-options(echo=TRUE)
-args <- commandArgs(trailingOnly = TRUE)
-
-# Subset by sample using commandline argument - uncomment code below if desired
-# pre_regressed_seurat <- SubsetData(pre_regressed_seurat,
-                                cells.use = rownames(pre_regressed_seurat@meta.data)[which(pre_regressed_seurat@meta.data$interestingGroups == args[1])])
 
 
 # Normalize counts for total cell expression and take log value                            
@@ -35,15 +26,12 @@ pre_regressed_seurat =  pre_regressed_seurat %>%
     dispersion.function = LogVMR,
     do.plot = FALSE)
 
-
 pre_regressed_seurat = pre_regressed_seurat %>%
   ScaleData(model.use = "linear")
 
 
 # Check number of variable genes to determine if correct parameters used  
-
 length(x = pre_regressed_seurat@var.genes)
-
 
 pre_regressed_seurat <- CellCycleScoring(
   pre_regressed_seurat,
@@ -55,4 +43,12 @@ pre_regressed_seurat = RunPCA(
   pc.genes = c(s_genes, g2m_genes),
   do.print = FALSE)
 
-saveRDS(pre_regressed_seurat, file = file.path(data_dir, paste0("seurat_pre_regress_", args[1], ".rds")))
+saveRDS(pre_regressed_seurat, file = file.path(data_dir, "seurat_pre_regress.rds"))
+
+# If needed, you could subset by sample using commandline argument at the beginning of the analysis - uncomment code below if desired
+# Use command line argument to specify sample extracted
+# options(echo=TRUE)
+# args <- commandArgs(trailingOnly = TRUE)
+#
+# pre_regressed_seurat <- SubsetData(pre_regressed_seurat,
+#                                cells.use = rownames(pre_regressed_seurat@meta.data)[which(pre_regressed_seurat@meta.data$interestingGroups == args[1])])
